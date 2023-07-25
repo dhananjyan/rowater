6dbf802d83ef7e34d8ae816455960befe1e2975a
Dhananjayan Sidhan <dhananjayan625@gmail.com>
2023-07-25 23:11:42 +0530

    Fix failed error

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ export default function About() {
         <div className="container">
             <h2 className="mt-5 pb-2">About us</h2>
             <p>Welcome to Vinayaga Engineering - Your Water Purification Specialists in Madurai.</p>
-            <p>At Vinayaga Engineering, we take immense pride in being Madurai's leading service providers in domestic and commercial water RO purifier installation, servicing, and spare parts provision. Our mission is simple yet powerful - to ensure you have access to the purest and safest drinking water, be it in the comfort of your home or your business.</p>
+            <p>At Vinayaga Engineering, we take immense pride in being Madurai&apos;s leading service providers in domestic and commercial water RO purifier installation, servicing, and spare parts provision. Our mission is simple yet powerful - to ensure you have access to the purest and safest drinking water, be it in the comfort of your home or your business.</p>
             <p>Why should you choose us? </p>
             <ul>
                 <li>Timely delivery & service</li>
