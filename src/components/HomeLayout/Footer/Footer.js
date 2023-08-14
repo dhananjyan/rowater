1924c92a633bada8abb155b82a848d3405a658f6
Dhananjayan <dhananjayan.s.n@doodleblue.in>
2023-08-14 15:41:23 +0530

    Images added and remove noIndex

--- src/components/HomeLayout/Footer/Footer.js
+++ src/components/HomeLayout/Footer/Footer.js
@@ -23,23 +23,33 @@ export default function Footer() {
                             <p>A water purification specialists in Madurai you can trust. At Vinayaga Engineering, we offer both commercial & domestic water purifier systems.
                             </p>
                             <div>
-                                <div role="button" onClick={() => router.push('/')} className={cx("pt-2")}>Request a call back</div>
-                                <div role="button" onClick={() => handleMenuClick("about-us")} className={cx("pt-2")}>About us</div>
-                                <div role="button" onClick={() => handleMenuClick("products")} className={cx("pt-2")}>Product we offer</div>
-                                <div role="button" onClick={() => handleMenuClick("wholesale")} className={cx("pt-2")}>Wholesale dealer of</div>
-                                <div role="button" onClick={() => handleMenuClick("services")} className={cx("pt-2")}>Service we undertake</div>
+                                <div role="button" onClick={() => router.push('/')} className={cx("pt-2")}><span className='me-2'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
+                                    <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z" />
+                                </svg></span>Request a call back</div>
+                                <div role="button" onClick={() => handleMenuClick("about-us")} className={cx("pt-2")}><span className='me-2'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
+                                    <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z" />
+                                </svg></span>About us</div>
+                                <div role="button" onClick={() => handleMenuClick("products")} className={cx("pt-2")}><span className='me-2'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
+                                    <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z" />
+                                </svg></span>Product we offer</div>
+                                <div role="button" onClick={() => handleMenuClick("wholesale")} className={cx("pt-2")}><span className='me-2'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
+                                    <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z" />
+                                </svg></span>Wholesale dealer of</div>
+                                <div role="button" onClick={() => handleMenuClick("services")} className={cx("pt-2")}><span className='me-2'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
+                                    <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z" />
+                                </svg></span>Service we undertake</div>
                             </div>
                         </div>
                         <div className="col col-12 col-md-4 col-lg-4 my-4">
                             <h5>Location we serve</h5>
                             <div>
-                                <div className={cx("pt-2")}>Water purifier service</div>
-                                <div className={cx("pt-2")}>Water leakage fixing</div>
-                                <div className={cx("pt-2")}>RO purifier noise & vibrations fixing</div>
-                                <div className={cx("pt-2")}>Excess reject water fixing</div>
-                                <div className={cx("pt-2")}>Low water flow</div>
-                                <div className={cx("pt-2")}>Auto shut-off failure</div>
-                                <div className={cx("pt-2")}>Bad taste of water</div>
+                                <div className={cx("pt-2")}>Madurai</div>
+                                <div className={cx("pt-2")}>Dindigul</div>
+                                <div className={cx("pt-2")}>Sivagangai</div>
+                                <div className={cx("pt-2")}>Virudhunagar</div>
+                                <div className={cx("pt-2")}>Theni</div>
+                                <div className={cx("pt-2")}>Ramanathapuram</div>
+                                <div className={cx("pt-2")}>Tirunelveli</div>
                             </div>
                         </div>
                         <div className="col col-12 col-md-4 col-lg-4 my-4">
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
-        <meta name="robots" content="noindex" />
         <link rel="canonical" href="https://vinayagawaterpurifiers.com/"/>
       </Head>
       <body>
