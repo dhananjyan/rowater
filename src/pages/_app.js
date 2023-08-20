b4779059c58337eebfda15ddd1df145a02fa7b4f
Dhananjayan <dhananjayan.s.n@doodleblue.in>
2023-08-20 06:43:16 +0530

    Single page update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import 'nprogress/nprogress.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import NProgress from "nprogress";
+import Router from "next/router";
+import Head from 'next/head';
+
+// NProgress.configure({ showSpinner: false });
+
+// Router.onRouteChangeStart = url => {
+//   NProgress.start()
+// }
+
+// Router.onRouteChangeComplete = () => NProgress.done()
+
+// Router.onRouteChangeError = () => NProgress.done()
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <link rel="canonical" href="https://www.vinayagawaterpurifiers.com/" />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs    /nprogress/0.2.0/nprogress.min.css"
+      />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
--- src/pages/product/[product-slug].js
+++ src/pages/product/[product-slug].js
@@ -3,6 +3,7 @@ import MainBanner from "@/components/MainBanner/MainBanner";
 import data from "../../assets/data.json";
 import Head from "next/head";
 import s from "../../styles/Home.module.css";
+import cx from "classname";
 
 export const getServerSideProps = async (ctx) => {
     const slug = ctx.query["product-slug"];
@@ -14,7 +15,7 @@ export default function SingleProduct({ result }) {
     return (
         <div className={s.parent}>
             <Head>
-                <title>{result.title} | Vinayaga Engineering</title>
+                <title>{`${result.metaTitle} | Vinayaga Engineering`}</title>
                 <meta
                     name="description"
                     content="We offer everything you need to get pure water from domestic RO water purifier, dispenser, CRI pumps, to industrial & commercial water plants & spare parts."
@@ -23,7 +24,12 @@ export default function SingleProduct({ result }) {
             </Head>
             <HomeLayout >
                 <MainBanner title={result.name} />
-
+                {result?.section?.map(section => {
+                    return (<div className={cx("container", "my-5")}>
+                        <h2>{section?.title}</h2>
+                        <div dangerouslySetInnerHTML={{__html: section?.content}} />
+                    </div>)
+                })}
             </HomeLayout>
         </div>
     )
