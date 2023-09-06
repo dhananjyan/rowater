00698c1a1f7917ba8936eb1c0a2996fea0cd16a9
Dhananjayan <dhananjayan.s.n@doodleblue.in>
2023-09-06 10:48:20 +0530

    Google tags added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,8 @@
 import '@/styles/globals.css';
 import 'nprogress/nprogress.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import NProgress from "nprogress";
-import Router from "next/router";
 import Head from 'next/head';
 
-// NProgress.configure({ showSpinner: false });
-
-// Router.onRouteChangeStart = url => {
-//   NProgress.start()
-// }
-
-// Router.onRouteChangeComplete = () => NProgress.done()
-
-// Router.onRouteChangeError = () => NProgress.done()
 
 export default function App({ Component, pageProps }) {
   return <>
@@ -23,7 +12,17 @@ export default function App({ Component, pageProps }) {
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs    /nprogress/0.2.0/nprogress.min.css"
       />
+      <script dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K7V4RFVQ');
+`}} />
+
     </Head>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7V4RFVQ"
+      height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
     <Component {...pageProps} />
   </>
 }
